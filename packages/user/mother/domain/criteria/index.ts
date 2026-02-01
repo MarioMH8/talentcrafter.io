acd78102be4b7862e0bc61edccfe35dfc20fb5e9
MarioMH8 <mhidalgomario@gmail.com>
2026-02-01 17:37:52 +0100

    refactor: remove FindUserByUsernameCriteria

--- packages/user/mother/domain/criteria/index.ts
+++ packages/user/mother/domain/criteria/index.ts
@@ -1,4 +1,3 @@
 export { default as FindUserByIdCriteriaMother } from './find-by-id.criteria.mother';
 export { default as FindUserByEmailCriteriaMother } from './find-by-user-email.criteria.mother';
-export { default as FindUserByUsernameCriteriaMother } from './find-by-username.criteria.mother';
 export { default as SearchUserByTermCriteriaMother } from './search-by-term.criteria.mother';
--- packages/user/src/domain/criteria/index.ts
+++ packages/user/src/domain/criteria/index.ts
@@ -1,14 +1,12 @@
 import FindUserByIdCriteria from './find-by-id.criteria';
 import FindUserByEmailCriteria from './find-by-user-email.criteria';
-import FindUserByUsernameCriteria from './find-by-username.criteria';
 import type SearchUserByTermCriteria from './search-by-term.criteria';
 
-type FindUserCriteria = FindUserByEmailCriteria | FindUserByIdCriteria | FindUserByUsernameCriteria;
+type FindUserCriteria = FindUserByEmailCriteria | FindUserByIdCriteria;
 type SearchUserCriteria = SearchUserByTermCriteria;
 
 export type { FindUserCriteria, SearchUserCriteria };
 
 export { default as FindUserByIdCriteria } from './find-by-id.criteria';
 export { default as FindUserByEmailCriteria } from './find-by-user-email.criteria';
-export { default as FindUserByUsernameCriteria } from './find-by-username.criteria';
 export { default as SearchUserByTermCriteria } from './search-by-term.criteria';
