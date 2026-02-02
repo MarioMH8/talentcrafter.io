7e0c2d5270621934b15dfaf3598e6a3760fc78a9
MarioMH8 <mhidalgomario@gmail.com>
2026-02-02 17:25:52 +0100

    fix(user): update error code prefix in CreateUserDuplicatedError

--- packages/user/src/application/create/create.use-case.error.ts
+++ packages/user/src/application/create/create.use-case.error.ts
@@ -2,6 +2,6 @@ import InvalidArgumentError from '@hexadrop/error/invalid-argument';
 
 export default class CreateUserDuplicatedError extends InvalidArgumentError {
 	constructor(message: string) {
-		super(message, 'CreateUserDuplicatedError', 'HUB(409)');
+		super(message, 'CreateUserDuplicatedError', 'USR(409)');
 	}
 }
